a310b0eaa59c50f7c895740371f4752a0bc15ac2
tatsuyafw <tatsuya7.hoshino7@gmail.com>
2012-08-14 08:19:04 +0900

    add Problem54

--- Problem54/Problem54.hs
+++ Problem54/Problem54.hs
@@ -12,13 +12,14 @@ data Hand =
     FK | -- Four of a Kind
     SF | -- Straight Flush
     RF   --- Royal Flush
-    deriving (Eq, Show)
-            
-data Value = TWO | Three | Four | Five | Six | Seven | Eight | Nine | Ten
+    deriving (Eq, Show, Ord)
+
+data Value = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten
           | Jack | Queen | King | Ace deriving (Eq, Show, Ord, Enum)
-type Kind = Char
+data Kind = H | D | S | C deriving (Eq, Show)
 data Card = Card Value Kind deriving (Eq, Show)
 type Player = (Card, Card, Card, Card, Card)
+type Rank = Maybe (Hand, [Value])
 
 value :: Card -> Value
 value (Card m _ ) = m
@@ -36,98 +37,205 @@ sameKind (x1:x2:xs) = if x1 == x2
                       then sameKind (x2:xs)
                       else False
 
-toRF :: Player -> Maybe Hand
+toRF :: Player -> Rank
 toRF ps = if isRF
-          then Just RF
+          then Just (RF, [])
           else Nothing
-    where 
+    where
       isRF = (sameKind $ kinds ps)
-             && (sort $ values ps)== rs
+             && (sort $ values ps) == rs
                  where rs = [Ten, Jack, Queen, King, Ace]
-                            
+
 isSF :: Player -> Bool
-isSF ps =
-    (sameKind $ kinds ps)
+isSF ps = (sameKind $ kinds ps)
     && if s >= Jack
        then False
        else ms == [s..(last ms)]
     where
       s = head ms
       ms = sort $ values ps
-toSF :: Player -> Maybe (Hand, Value)
+toSF :: Player -> Rank
 toSF ps = if isSF ps
-          then Just (SF, hi)
+          then Just (SF, [hi])
           else Nothing
               where
                 hi = last . sort $ values ps
-                                
+
 isFK :: Player -> Bool
-isFK ps = any gtFour . map (\xs -> (length xs, head xs)) $ group ms
+isFK ps = any (>=4) . map length $ group ms
     where
-      gtFour (n, _) = n >= 4
       ms = sort $ values ps
-toFK :: Player -> Maybe (Hand, Value, Value)
+toFK :: Player -> Rank
 toFK ps = if isFK ps
-          then Just (FK, v1, v2)
+          then Just (FK, vs)
           else Nothing
               where
-                v1 = snd (xs !! 1)
-                v2 = snd (xs !! 0)
-                xs = sort . map (\xs -> (length xs, head xs)) $ group ms
+                vs = map snd xs
+                xs = reverse . sort . map (\xs -> (length xs, head xs)) $ group ms
                 ms = sort $ values ps
 
 isFH :: Player -> Bool
-isFH ps = twoAndThree . map (\xs -> (length xs, head xs)) $ group ms
-    where 
-      twoAndThree xs = (map (\ys -> fst ys) $ sort xs) == [2, 3]
+isFH ps = twoAndThree . map length $ group ms
+    where
+      twoAndThree xs = sort xs == [2, 3]
       ms = sort $ values ps
-toFH :: Player -> Maybe (Hand, Value, Value)
+toFH :: Player -> Rank
 toFH ps = if isFH ps
-          then Just (FH, v1, v2)
+          then Just (FH, vs)
           else Nothing
               where
-                v1 = snd (xs !! 1)
-                v2 = snd (xs !! 0)
-                xs = sort . map (\xs -> (length xs, head xs)) $ group ms
+                vs = map snd xs
+                xs = reverse . sort . map (\xs -> (length xs, head xs)) $ group ms
                 ms = sort $ values ps
 
 isFL :: Player -> Bool
 isFL ps = sameKind $ kinds ps
-toFL :: Player -> Maybe (Hand, Value, Value, Value, Value, Value)
+toFL :: Player -> Rank
 toFL ps = if isFL ps
-          then Just (FL, ms !! 4, ms !! 3, ms !! 2, ms !! 1, ms !! 0)
+          then Just (FL, vs)
           else Nothing
               where
-                ms = sort $ values ps
+                vs = reverse . sort $ values ps
 
 isST :: Player -> Bool
 isST ps = ms == [(head ms)..(last ms)]
     where
       ms = sort $ values ps
-toST :: Player -> Maybe (Hand, Value, Value, Value, Value, Value)
+toST :: Player -> Rank
 toST ps = if isST ps
-          then Just (ST, ms !! 4, ms !! 3, ms !! 2, ms !! 1, ms !! 0)
+          then Just (ST, ms)
           else Nothing
               where
-                ms = sort $ values ps
+                ms = reverse . sort $ values ps
 
 isTK :: Player -> Bool
-isTK ps = oneAndOneAndThree . map (\xs -> (length xs, head xs)) $ group ms
+isTK ps = oneAndOneAndThree . map length $ group ms
     where
-      oneAndOneAndThree xs = (map (\xs -> fst xs) $ sort xs) == [1, 1, 3]
+      oneAndOneAndThree xs = sort xs == [1, 1, 3]
       ms = sort $ values ps
-toTK :: Player -> Maybe (Hand, Value, Value, Value)
+toTK :: Player -> Rank
 toTK ps = if isTK ps
-          then Just (TK, v1, v2, v3)
+          then Just (TK, xs)
           else Nothing
               where
-                v1 = snd (xs !! 2)
-                v2 = snd (xs !! 1)
-                v3 = snd (xs !! 0)
-                xs = sort . map (\xs -> (length xs, head xs)) $ group ms
+                xs = reverse . sort . map head $ group ms
                 ms = sort $ values ps
 
 isTP :: Player -> Bool
-isTP ps = 
+isTP ps = oneAndTwoAndTwo . map length $ group ms
+    where
+      oneAndTwoAndTwo xs = sort xs == [1, 2, 2]
+      ms = sort $ values ps
+toTP :: Player -> Rank
+toTP ps = if isTP ps
+          then Just (TP, [v0])
+          else Nothing
+              where
+                v0 = maximum [v | x <- xs, let (l, v) = x, l == 2]
+                v1 = minimum [v | x <- xs, let (l, v) = x, l == 2]
+                v2 = minimum . map snd $ xs
+                xs = reverse . sort . map (\xs -> (length xs, head xs)) $ group ms
+                ms = sort $ values ps
 
-problem54 = 1                                
+isOP :: Player -> Bool
+isOP ps = oneAndOneAndOneAndTwo . map length $ group ms
+    where
+      oneAndOneAndOneAndTwo xs = sort xs == [1, 1, 1, 2]
+      ms = sort $ values ps
+toOP :: Player -> Rank
+toOP ps = if isOP ps
+          then Just (OP, v0 : vs)
+          else Nothing
+              where
+                v0 = snd . head $ xs
+                vs = reverse . sort . map snd . tail $ xs
+                xs = reverse . sort . map (\xs -> (length xs, head xs)) $ group ms
+                ms = sort $ values ps
+
+isHC :: Player -> Bool
+isHC ps = noPair . map length $ group ms
+    where
+      noPair xs = sort xs == [1, 1, 1, 1, 1]
+      ms = sort $ values ps
+toHC :: Player -> Rank
+toHC ps = if isHC ps
+          then Just (HC, ms)
+          else Nothing
+              where
+                ms = reverse . sort $ values ps
+
+judgeRank :: Player -> Rank
+judgeRank ps = head . dropWhile (== Nothing) . reverse . map (\f -> f ps) $ handList
+    where handList = [toHC, toOP, toTP, toTK, toST, toFL, toFH, toFK, toSF, toRF]
+
+compValue :: [Value] -> [Value] -> Ordering
+compValue (v1:vs1) (v2:vs2) = if v1 == v2
+                               then compValue vs1 vs2
+                               else v1 `compare` v2
+
+toHand :: Rank -> Hand
+toHand (Just (h, _)) = h
+
+compRank :: Rank -> Rank -> Ordering
+compRank r1 r2 = if h1 == h2
+                 then compValue (toValue r1) (toValue r2)
+                 else h1 `compare` h2
+                     where
+                       h1 = toHand r1
+                       h2 = toHand r2
+                       toHand (Just (h, _)) = h
+                       toValue (Just (_, vs)) = vs
+
+compPlayer :: Player -> Player -> Ordering
+compPlayer p1 p2 = compRank r1 r2
+    where
+      r1 = judgeRank p1
+      r2 = judgeRank p2
+
+readValue :: Char -> Value
+readValue c
+    | c == '2' = Two
+    | c == '3' = Three
+    | c == '4' = Four
+    | c == '5' = Five
+    | c == '6' = Six
+    | c == '7' = Seven
+    | c == '8' = Eight
+    | c == '9' = Nine
+    | c == 'T' = Ten
+    | c == 'J' = Jack
+    | c == 'Q' = Queen
+    | c == 'K' = King
+    | otherwise = Ace
+
+readKind :: Char -> Kind
+readKind c
+    | c == 'H'  = H
+    | c == 'D'  = D
+    | c == 'S'  = S
+    | otherwise = C
+
+readCard :: String -> Card
+readCard vk = Card (readValue v) (readKind k)
+    where
+      v = vk !! 0
+      k = vk !! 1
+
+readPlayer :: [String] -> Player
+readPlayer xs = (c0, c1, c2, c3, c4)
+    where
+      c0 = readCard (xs !! 0)
+      c1 = readCard (xs !! 1)
+      c2 = readCard (xs !! 2)
+      c3 = readCard (xs !! 3)
+      c4 = readCard (xs !! 4)
+
+problem54 = do
+  lines <- fmap lines getContents
+  let contents = map (splitAt 5 . words) lines
+      playerMatches = map (\(ps1, ps2) -> ((readPlayer ps1), (readPlayer ps2))) contents
+      battles = map (\(p1, p2) -> compPlayer p1 p2) playerMatches
+      player1Wins = length . filter (== GT) $ battles
+  print player1Wins
+
+main = problem54
